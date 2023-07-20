7f382be0a2f2e4b0e0c0d88a2cd57f58a1024416
KV5d <berrigannolan@gmail.com>
2023-07-20 11:48:40 -0600

    first commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+const api = "https://ip-api.com";
+const fields = {
+    coun: "country",
+    cont: "continent",
+    prov: "regionName",
+    city: "city",
+    post: "zip",
+    lon: "lon",
+    lat: "lat",
+    tz: "timzone",
+    isp: "isp",
+    curr: "currency",
+    mob: "mobile",
+    prox: "proxy"
+};
---
+++ todo.md
@@ -0,0 +1,7 @@
+# **To-Do's**
+
+- pull ip data from api
+- give google maps link to coordinates
+- prettify everything
+- create command line arguments with flags
+- compile into binary
