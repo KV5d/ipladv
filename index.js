fae116a25476597e00dfa72a49fdb27ed57fa417
KV5d <berrigannolan@gmail.com>
2023-07-20 23:48:22 -0600

    .

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
-var hello = "Hello World";
-console.log(hello);
+"use strict";
+exports.__esModule = true;
+var process_1 = require("process");
+if (process.argv.length > 3) {
+    console.log("excess argument(s) supplied | HINT: only pass one argument");
+    (0, process_1.exit)();
+}
+else if (process.argv.length < 3) {
+    console.log("insufficient arguments supplied | HINT: you must pass one argument");
+    (0, process_1.exit)();
+}
+else {
+    ;
+}
+var target = process.argv[2];
+var api = "http://ip-api.com/json/" + target + "?fields=9634809";
+var data = '';
+// google maps link base: https://maps.google.com/?ll=latitude,longitude
+fetch(api).then(function (response) {
+    return response.json();
+}).then(function (jsonData) {
+    data = jsonData;
+    // console.log(data);
+    var dataStringify = JSON.stringify(data);
+    var obj = JSON.parse(dataStringify);
+    var lon = obj.lon;
+    var lat = obj.lat;
+    var cont = obj.continent;
+    var coun = obj.country;
+    var prov = obj.regionName;
+    var city = obj.city;
+    var zip = obj.zip;
+    var tz = obj.timezone;
+    var curr = obj.currency;
+    var isp = obj.isp;
+    var mob = obj.mobile;
+    var pro = obj.proxy;
+    console.log("\ncontinent: " + cont + "\ncountry: " + coun + "\nprovince/state: " + prov +
+        "\ncity: " + city + "\nzip: " + zip + "\ntimezone: " + tz + "\ncurrency: " + curr +
+        "\nisp: " + isp + "\nmobile: " + mob + "\nproxy: " + pro);
+    var gmap = "\nhttps://maps.google.com/?ll=" + lat + "," + lon;
+    console.log(gmap + "\n");
+});
--- index.ts
+++ index.ts
@@ -1,15 +1,53 @@
-const api = "https://ip-api.com";
-const fields = {
-    coun: "country",
-    cont: "continent",
-    prov: "regionName",
-    city: "city",
-    post: "zip",
-    lon: "lon",
-    lat: "lat",
-    tz: "timzone",
-    isp: "isp",
-    curr: "currency",
-    mob: "mobile",
-    prox: "proxy"
-};
+import { exit } from "process";
+
+if (process.argv.length > 3) {
+    console.log("excess argument(s) supplied | HINT: only pass one argument");
+    exit();
+} else if (process.argv.length < 3) {
+    console.log("insufficient arguments supplied | HINT: you must pass one argument")
+    exit();
+} else {
+    ;
+}
+
+let target = process.argv[2];
+let api = "http://ip-api.com/json/" + target + "?fields=9634809";
+let data  = '';
+
+// google maps link base: https://maps.google.com/?ll=latitude,longitude
+
+fetch(api).then(function(response) {
+    return response.json();
+ }).then(function(jsonData) {
+    data=jsonData
+    // console.log(data);
+
+    let dataStringify = JSON.stringify(data);
+
+    let obj = JSON.parse(dataStringify);
+    let lon = obj.lon;
+    let lat = obj.lat;
+    let cont = obj.continent;
+    let coun = obj.country;
+    let prov = obj.regionName;
+    let city = obj.city;
+    let zip = obj.zip;
+    let tz = obj.timezone;
+    let curr = obj.currency;
+    let isp = obj.isp;
+    let mob = obj.mobile;
+    let pro = obj.proxy;
+
+    console.log("\ncontinent: " + cont + "\ncountry: " + coun + "\nprovince/state: " + prov + 
+        "\ncity: " + city + "\nzip: " + zip + "\ntimezone: " + tz + "\ncurrency: " + curr + 
+        "\nisp: " + isp + "\nmobile: " + mob + "\nproxy: " + pro);
+
+
+    let gmap = "\nhttps://maps.google.com/?ll=" + lat + "," + lon;
+    console.log(gmap + "\n");
+
+});
+
+
+
+
